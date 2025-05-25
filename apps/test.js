9a810cbadc4e266b8b49920d6df00a914f2da384
khromeengine <jianhengzhou003@gmail.com>
2025-05-24 21:24:35 -0700

    made test file

---
+++ apps/test.js
@@ -0,0 +1,8 @@
+const testbox = d3.select("svg")
+    .append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 500)
+        .attr("height", 600)
+
+export default testbox;
---
+++ main.js
@@ -0,0 +1,1 @@
+import testbox from "./apps/test.js"
