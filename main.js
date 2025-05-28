d1f17bca35de0987bb27ac716ce3c18fcab0d499
khromeengine <jianhengzhou003@gmail.com>
2025-05-27 18:08:56 -0700

    there's no way this would fix anything but worth a shot

--- main.js
+++ main.js
@@ -7,37 +7,41 @@ import testmakeSlide from "./apps/testmakeslide.js"
 let width = window.innerWidth;
 let height = window.innerHeight;
 
-let a = new Slide(width, height)
-let b = testmakeSlide([0, height/2, width/2, height/2, "magenta"]);
-let c = testmakeSlide([width/2, 0, width/2, height/2, "cyan"]);
-let d = testmakeSlide([width/2, height/2, width/2, height/2, "yellow"]);
 
-let streamgraph = new StreamGraph(a.base)
-console.log(Object.keys({width: 500, height: 500}))
+
+d3.csv("./data/vgsales.csv").then(data => {
+    let a = new Slide(width, height)
+    let b = testmakeSlide([0, height/2, width/2, height/2, "magenta"]);
+    let c = testmakeSlide([width/2, 0, width/2, height/2, "cyan"]);
+    let d = testmakeSlide([width/2, height/2, width/2, height/2, "yellow"]);
+
+    let streamgraph = new StreamGraph(a.base)
+    console.log(Object.keys({width: 500, height: 500}))
     streamgraph.resizeStream({width: 1200, height: 500})
     streamgraph.moveStream({x: 20, y: 200})
     streamgraph.defineStreamMargins({top: 50, left: 100, bottom: 50, right: 50})
 
-d3.csv("./data/vgsales.csv").then(data => {
     streamgraph.initDataset(data);
     console.log(streamgraph.dataset)
     streamgraph.calculateRegion("NA");
     streamgraph.drawRegion("NA");
-})
 
-
-let slides = new Slider([a, b, c, d], 200, [-width, 0, width])
-
-
-
-d3.select("svg")
+    let slides = new Slider([a, b, c, d], 200, [-width, 0, width])
+    d3.select("svg")
     .on("click", function() { slides.goNext(); })
     .on("contextmenu", function() { 
         d3.event.preventDefault(); 
         slides.goPrev(); 
     });
 
-// each butt action
-document.getElementById('zoom-in')?.addEventListener('click', function() {
-    console.log('Zoom in clicked on stream graph');
-});
+    // each butt action
+    document.getElementById('zoom-in')?.addEventListener('click', function() {
+        console.log('Zoom in clicked on stream graph');
+    });
+})
+
+
+
+
+
+
